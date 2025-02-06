4497afc668d74029ad726c0d282733b936dce5e9
Ivet Lopez <ivet19@live.com>
2025-02-06 20:19:47 +0100

    first commit

---
+++ school-report.js
@@ -0,0 +1,62 @@
+const firstStudentName = "Alex González";
+const secondStudentName = "Marina Sierra";
+
+const firstStudentAge = 29;
+const secondStudentAge = 34;
+
+const firstCourseName = "Introducción al punto de cruz";
+const secondCourseName = "Patronaje técnico";
+const thirdCourseName = "Costura creativa y reciclaje";
+
+const firstDifficultyLevel = "básico";
+const secondDifficultyLevel = "intermedio";
+const thirdDifficultyLevel = "avanzado";
+
+const firstCourseDuration = 12;
+const secondCourseDuration = 60;
+const thirdCourseDuration = 25;
+
+const studentsAverageAge = (firstStudentAge + secondStudentAge) / 2;
+
+console.log(
+  "INFORME DE LA ESCUELA" +
+    "\n " +
+    "\nCursos:" +
+    "\n- " +
+    firstCourseName +
+    " (nivel " +
+    firstDifficultyLevel +
+    ", " +
+    firstCourseDuration +
+    " horas)" +
+    "\n- " +
+    secondCourseName +
+    " (nivel " +
+    thirdDifficultyLevel +
+    ", " +
+    thirdCourseDuration +
+    " horas)" +
+    "\n- " +
+    thirdCourseName +
+    " (nivel " +
+    secondDifficultyLevel +
+    ", " +
+    thirdCourseDuration +
+    " horas)" +
+    "\n " +
+    "\nAlumnos:" +
+    "\n- " +
+    firstStudentName +
+    ", " +
+    firstStudentAge +
+    " años" +
+    "\n- " +
+    secondStudentName +
+    ", " +
+    secondStudentAge +
+    " años" +
+    "\n " +
+    "\nEdad media de los alumnos : " +
+    studentsAverageAge +
+    " años"
+);
